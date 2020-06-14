b25fda7d38ead73673aed3233fb6d54e303d1567
Andrew Song <andrewsong.j@gmail.com>
2020-06-14 15:56:31 -0400

    caching project

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,30 @@
+## Creates the special matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## Returns and caches (if necessary) the inverse of the matrix 
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  matrx <- x$get()
+  i <- solve(matrx)
+  x$setinv(i)
+  i
+}
